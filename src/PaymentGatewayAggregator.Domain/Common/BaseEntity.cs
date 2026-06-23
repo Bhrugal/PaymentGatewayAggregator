674d1d2f92667a733ee410fffa35bb0f8f44a6e4
Bhrugal Patel <bhrugalpatel2508@gmail.com>
2026-06-23 23:20:18 +0530

    Added domain entities and enums

---
+++ src/PaymentGatewayAggregator.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace PaymentGatewayAggregator.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+}
